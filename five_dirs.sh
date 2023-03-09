63a51b483b595dc62351d8136fee2367b767f580
Mao Ruirui <rmao28@wisc.edu>
2023-03-09 11:59:36 -0600

    write five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+mkdir five
+for i in $(seq 5); do
+	mkdir ./five/dir$i;
+	for j in $(seq 4);do
+#	echo >> ./five/dir$i/file$j;
+		for k in $(seq $j);do
+			echo $j >> ./five/dir$i/file$j;
+		done;
+	done;
+done;
+
