1b53a5cc71b344713e71a22e4da40b9e5887cc98
Mao Ruirui <rmao28@wisc.edu>
2023-03-09 12:00:21 -0600

    write rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,23 @@
-#!/bin/bash line
+#!/bin/bash
+
+# Usage statement
+usage() {
+    echo "usage: $0 <dir> <n>" 1>&2
+}
+
+# Check for correct number of arguments
+if [ $# -ne 2 ]; then
+    usage
+    exit 1
+fi
+
+# Check if directory exists
+if [ ! -d "$1" ]; then
+    echo "$1: No such directory" 1>&2
+    usage
+    exit 1
+fi
+
+# Remove files larger than n bytes
+find "$1" -type f -size "+${2}c" -delete
+
