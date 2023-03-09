6b9d883fae527dbef449cc43ee48b9ca068ff269
hank1234562 <sli826@wisc.edu>
2023-03-09 09:45:45 -0600

    get the mean

--- mean.sh
+++ mean.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "usage: $0 <column> [file.csv]" >&2
+  exit 1
+fi
+
+column="$1"
+file="${2:-/dev/stdin}"
+
+sum=0
+count=0
+
+cut -d, -f"$column" "$file" | tail -n+2 | {
+  while read value; do
+    sum=$(echo "$sum + $value" | bc)
+    count=$((count + 1))
+  done
+
+  if [ "$count" -gt 0 ]; then
+    mean=$(echo "scale=10; $sum / $count" | bc)
+    echo "$mean"
+  else
+    echo "error: no data" >&2
+    exit 1
+  fi
+}
+
